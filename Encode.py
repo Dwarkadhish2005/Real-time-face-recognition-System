f92ae6c2dc696a6167d122741265c1d1918e7ab4
Dwarkadhish Kamthane <72198604+Dwarkadhish2005@users.noreply.github.com>
2025-02-13 22:36:25 +0530

    Update Encode.py
    removed key

--- Encode.py
+++ Encode.py
@@ -1,51 +1,3 @@
-# import cv2
-# import os
-# import face_recognition
-# import pickle
-#
-# import firebase_admin
-# from firebase_admin import credentials
-# from  firebase_admin import db
-# from firebase_admin import storage
-#
-# cred = credentials.Cerificate("servicekey.json")
-# firebase_admin.initialize_app(cred , {
-# 'databaseURL':"",
-# 'storageBucket':""
-# })
-# folderPath = 'C:/Users/Dwarkadhish Kamthane/Desktop/Machiene Learning/Real-time face recognition and attendance system/images'
-# pathList = os.listdir(folderPath)
-# # print(pathList)
-# imgList = []
-# studentIds =[]
-# for path in pathList:
-#     imgList.append(cv2.imread(os.path.join(folderPath,path)))
-#     # print(os.path.splitext(path)[0])
-#     studentIds.append(os.path.splitext(path)[0])
-#
-#     fileName = os.path.join(folderPath , path)
-#     bucket = storage.bucket()
-#     blob = bucket.blob(fileName)
-#     blob.upload_from_filename(fileName)
-# print(studentIds)
-#
-# def findEncodings(imagesList):
-#     encodeList =[]
-#     for img in imagesList:
-#         img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB)
-#         encode = face_recognition.face_encodings(img)[0]
-#         encodeList.append(encode)
-#
-#     return encodeList
-# print("Encoding Started.....")
-# encodeListKnown = findEncodings(imgList)
-# encodeListKnownWithIds = [encodeListKnown , studentIds]
-# print("Encoding Complete")
-#
-# file = open("EncodeFile.p",'wb')
-# pickle.dump(encodeListKnownWithIds , file)
-# file.close()
-# print("File Saved ")
 
 import cv2
 import os
@@ -57,7 +9,7 @@ from supabase import create_client, Client
 
 # Supabase Credentials
 SUPABASE_URL = "https://xccejnrpvhnqbzpjymvz.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjY2VqbnJwdmhucWJ6cGp5bXZ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODYzMTcyNSwiZXhwIjoyMDU0MjA3NzI1fQ.KFj9OJ13Ax5rTC-nE6RcxL4eAjdQEpiL5bBgZeOAQZ4"
+SUPABASE_KEY = "put your own key here"
 
 # Initialize Supabase Client
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
